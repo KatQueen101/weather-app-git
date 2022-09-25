8012da959ccc440e2b02581aabc7e3cf0b9c149d
KatQueen101 <brenae1999@hotmail.com>
2022-09-25 15:49:44 -0400

    Updated

--- src/index.js
+++ src/index.js
@@ -124,7 +124,7 @@ function displayForecast(response) {
         alt=""
         width="42"
       />
-      <div class="weather-forcast-temperatures">
+      <div class="weather-forecast-temperatures">
         <span class="weather-forecast-temperature-max"> ${Math.round(
           forecastDay.temp.max
         )}° </span>
