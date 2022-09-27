0916be4dc96b265f93622b9c384dae5e373f4cf6
KatQueen101 <brenae1999@hotmail.com>
2022-09-27 12:27:32 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -44,6 +44,9 @@ function handleSubmit(event) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
 }
 
 function showTemperature(response) {
