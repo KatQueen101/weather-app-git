70a8ed50147d0d5d96dbd3c736f0ba08dd9743e8
KatQueen101 <brenae1999@hotmail.com>
2022-09-27 12:36:39 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ function formatDate(date) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
+  console.log(response.data.daily);
 }
 
 function displayWeatherCondition(response) {
