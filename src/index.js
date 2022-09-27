ca822b395bd8311affb1ce2607a2c43d4032d156
KatQueen101 <brenae1999@hotmail.com>
2022-09-27 13:06:47 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,12 +23,16 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function getForecast(coords) {
+function displayForecast(response) {
+  console.log(response.data.daily);
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let lon = coords.lon;
-  let lat = coords.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayWeatherCondition(response) {
