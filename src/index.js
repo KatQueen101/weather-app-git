6f04b69a051a26fc9d1699eb99a694f033e9ed7c
KatQueen101 <brenae1999@hotmail.com>
2022-09-27 13:20:47 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&appid=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
